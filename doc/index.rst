dc6fc13ee86d13c8885f7407886d65fa94a6a7c4
Vova Abdrakhmanov <369565@gmail.com>
2018-01-04 12:42:27 +0300

    start using Sphinx

---
+++ doc/index.rst
@@ -0,0 +1,23 @@
+.. smoothie documentation master file, created by
+   sphinx-quickstart on Thu Jan  4 11:03:15 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to smoothie's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: loess
+   :members:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- loess.py
+++ loess.py
@@ -2,20 +2,32 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def weighted_less_squares(x, y, weights, degree, point):
+    """
+    Calculate weighted less squares approximation in given point
+    """
     fit = np.polyfit(x, y, degree, w=weights)
     return np.polyval(fit, point)
 
 def bisquare(x):
+    """
+    Bisquare weight function :math:`w(x)=(1-x^2)^2`
+    """
     result = 1 - x * x
     result[result < 0] = 0
     return result ** 2
 
 def tricube(x):
+    """
+    Tricube weight function :math:`w(x)=(1-|x|^3)^3`
+    """
     result = 1 - np.abs(x ** 3)
     result[result < 0] = 0
     return result ** 3
 
 def loess(x, y, fraction=2/3, degree=2, weight_function=bisquare, iterations=3):
+    """
+    LOESS -- LOcal regrESSion scatterplot smoothing, according to Cleveland (1979)
+    """
     x, y = np.asarray(x), np.asarray(y)
     domain = x.max() - x.min()
     halfwidth = domain * fraction / 2
@@ -33,20 +45,21 @@ def loess(x, y, fraction=2/3, degree=2, weight_function=bisquare, iterations=3):
             new_y[k] = weighted_less_squares(x, y, weights[k,:], degree, xk)
     return new_y
 
-x = np.linspace(0, 5*np.pi, 100)
-signal = 10 * np.exp(-x/3)
-noise = np.random.rand(len(signal)) * 2 - 1
-y = signal + noise
-plt.title("LOESS (3 iterations)")
-plt.plot(x, signal, "b-", lw=2, label="signal")
-plt.plot(x, y, "b+", label="signal+noise")
-# smoothed = loess(x, y, 0.7, 0, bisquare)
-# plt.plot(x, smoothed, "m-", label="loess (d = 0, f = 0.7)")
-smoothed = loess(x, y, 0.7, 1, bisquare)
-plt.plot(x, smoothed, "g--", label="loess (d = 1, f = 0.7)")
-smoothed = loess(x, y, 0.7, 2, bisquare)
-plt.plot(x, smoothed, "r-", label="loess (d = 2, f = 0.7)")
-smoothed = loess(x, y, 0.7, 3, bisquare)
-plt.plot(x, smoothed, "k--", label="loess (d = 3, f = 0.7)")
-plt.legend(loc=1)
-plt.show()
+if __name__ == "__main__":
+    x = np.linspace(0, 5*np.pi, 100)
+    signal = 10 * np.exp(-x/3)
+    noise = np.random.rand(len(signal)) * 2 - 1
+    y = signal + noise
+    plt.title("LOESS (3 iterations)")
+    plt.plot(x, signal, "b-", lw=2, label="signal")
+    plt.plot(x, y, "b+", label="signal+noise")
+    # smoothed = loess(x, y, 0.7, 0, bisquare)
+    # plt.plot(x, smoothed, "m-", label="loess (d = 0, f = 0.7)")
+    smoothed = loess(x, y, 0.7, 1, bisquare)
+    plt.plot(x, smoothed, "g--", label="loess (d = 1, f = 0.7)")
+    smoothed = loess(x, y, 0.7, 2, bisquare)
+    plt.plot(x, smoothed, "r-", label="loess (d = 2, f = 0.7)")
+    smoothed = loess(x, y, 0.7, 3, bisquare)
+    plt.plot(x, smoothed, "k--", label="loess (d = 3, f = 0.7)")
+    plt.legend(loc=1)
+    plt.show()
